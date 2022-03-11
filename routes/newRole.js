9461fef1459724cca248edcbb55b97e118c4502c
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-03-11 00:12:56 -0800

    updated addRoles to insert new role into table

--- routes/newRole.js
+++ routes/newRole.js
@@ -22,37 +22,32 @@ const addRole = (role) =>
         .catch((error) => console.error('Error:', error))
 
 newRoleRouter.post('/', ({ body }, res) => {
-    let departmentId
-
     const getIdsql = `SELECT * FROM departments
-    WHERE department_name='${body.addDepartment}'`
-
+    WHERE department_name='${body.chooseDepartment}'`
     //Query to retrieve department_id 
     db.query(getIdsql, (err, result, rows) => {
         if (err) console.error(err)
         else {
-            console.log("rows", `${cTable.getTable(rows)}`)
-            departmentId = result
-            console.log("result", result)
+            console.log(result[0])
+            let { department_id, department_name } = result[0]
+
+            const sql = `INSERT INTO titles
+            (title_name, salary, department_id)
+            VALUES ("${body.addTitle}", ${parseInt(body.addSalary)}, ${department_id})`
+        
+            db.query(sql, (err, result) => {
+                if (err) console.error(err)
+        
+                //Sends a json response containing a success note and the information of the role added.
+                res.json({
+                    message: 'success',
+                    data: body
+                })
+        
+                //Logs the table of titles to the user's console for viewing.
+                console.log(`\r\n${cTable.getTable(result)}`)
+            })
         }
-    })
-    console.log("department id", departmentId)
-
-    const sql = `INSERT INTO titles
-    (title_name, salary, department_id)
-    VALUES (${body.addTitle}, ${parseInt(body.addSalary)}, ${departmentId})`
-
-    db.query(sql, (err, result) => {
-        if (err) throw err
-
-        //Sends a json response containing a success note and the information of the role added.
-        res.json({
-            message: 'success',
-            data: body
-        })
-
-        //Logs the table of titles to the user's console for viewing.
-        console.log(`\r\n${cTable.getTable(result)}`)
     })
 })
 
